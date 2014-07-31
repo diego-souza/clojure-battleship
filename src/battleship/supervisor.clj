6ab4546f7b9ffd6e14091cf168808ffcac688391
Diego Alves de Souza <diegosouza.mail@gmail.com>
2014-07-30 21:49:29 -0300

    Added supervisor namespace with rules for placing ships and shooting.

---
+++ src/battleship/supervisor.clj
@@ -0,0 +1,22 @@
+(ns battleship.supervisor
+  (:require [battleship.vec :as vec])
+  (:require [battleship.core :as c])
+  (:require [clojure.set :refer :all]))
+
+(defn inside-board? [[x y]]
+  (and (>= x 0) (>= y 0) (<= x 9) (<= y 9)))
+
+(defn can-place-ship? [game player pos size direction]
+  (let [ships              (-> game player :ships)
+        coords             (c/calculate-coords pos size direction)
+        has-space?         (empty? (intersection ships coords))
+        all-inside-board?  (empty? (remove inside-board? coords))
+        can?               (and has-space? all-inside-board?)]
+    can?))
+
+
+(defn can-shoot? [game player pos]
+  (let [shots              (-> game player :shots)
+        new-target?        (not (contains? shots pos))
+        can?               (and (inside-board? pos) new-target?)]
+    can?))
---
+++ test/battleship/supervisor_test.clj
@@ -0,0 +1,73 @@
+(ns battleship.supervisor-test
+  (:require [clojure.test :refer :all]
+            [battleship.core :as c]
+            [battleship.supervisor :refer :all]))
+
+(deftest possibility-of-placing-ships
+  (testing "Placing one ship next to other"
+    (let [g (-> (c/make-game)
+                (c/place-ship :player1 [1 1] 1 :north))]
+      (is (= true (can-place-ship? g :player1 [2 2] 1 :north)))))
+
+  (testing "Placing one ship above other"
+    (let [g (-> (c/make-game)
+                (c/place-ship :player1 [1 1] 1 :north))]
+      (is (= false (can-place-ship? g :player1 [1 1] 1 :north)))))
+
+
+  (testing "Placing ship inside the board"
+    (let [g (c/make-game)]
+      (are [coord]
+           (= true (can-place-ship? g :player1 coord 1 :north))
+           [0 0]
+           [1 1]
+           [4 5]
+           [9 9])))
+
+  (testing "Placing ship outside the board"
+    (let [g (c/make-game)]
+      (are [coord]
+           (= false (can-place-ship? g :player1 coord 1 :north))
+           [0 -1]
+           [-1 0]
+           [-1 -1]
+           [10 4]
+           [4 10]
+           [10 10]))))
+
+(deftest possibility-of-shooting
+    (testing "Placing one ship next to other"
+    (let [g (-> (c/make-game)
+                (c/place-ship :player1 [1 1] 1 :north))]
+      (is (= true (can-place-ship? g :player1 [2 2] 1 :north)))))
+
+  (testing "shooting next to previous shot"
+    (let [g (-> (c/make-game)
+                (c/shoot :player1 [0 0]))]
+      (is (= true (can-shoot? g :player1 [0 1])))))
+
+
+  (testing "shooting in the same place"
+    (let [g (-> (c/make-game)
+                (c/shoot :player1 [0 0]))]
+      (is (= false (can-shoot? g :player1 [0 0])))))
+
+  (testing "Shooting inside the board"
+    (let [g (c/make-game)]
+      (are [coord]
+           (= true (can-shoot? g :player1 coord))
+           [0 0]
+           [1 1]
+           [4 5]
+           [9 9])))
+
+  (testing "Shooting outside the board"
+    (let [g (c/make-game)]
+      (are [coord]
+           (= false (can-shoot? g :player1 coord))
+           [0 -1]
+           [-1 0]
+           [-1 -1]
+           [10 4]
+           [4 10]
+           [10 10]))))
